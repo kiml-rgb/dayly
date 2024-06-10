d3830867c92237db679e26a06e30655db75f89d6
kiml <kiml@users.noreply.github.com>
2024-06-10 15:14:51 +0800

    feat: day21题目

---
+++ src/main/java/camp/day21/LE77.java
@@ -0,0 +1,48 @@
+package camp.day21;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ZYF
+ * @date: 2024/6/10 9:58
+ * @description:
+ */
+public class LE77 {
+    public static void main(String[] args) {
+//        combine(4, 2);
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        combineBFS(1, n, k, list, result);
+        return result;
+    }
+
+    /**
+     *
+     * @param start 当前开始节点
+     * @param n n
+     * @param k k
+     * @param list 每个list
+     * @param result 最后的结果
+     */
+    private void combineBFS(int start, int n, int k, List<Integer> list, List<List<Integer>> result) {
+        if (list.size() == k) {
+            List<Integer> resultOne = new ArrayList<>(k);
+            resultOne.addAll(list);
+            result.add(resultOne);
+            return;
+        }
+
+        for (int j = start; j <= (n - (k - list.size())) + 1; j++) {
+            list.add(j);
+            combineBFS(j + 1, n, k, list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+}
+
