5bb510696289ca38b8e1422de1d2db1903a4bd49
kiml <kiml@users.noreply.github.com>
2024-06-11 16:12:18 +0800

    feat: day24题目

---
+++ src/main/java/camp/day24/LE78.java
@@ -0,0 +1,49 @@
+package camp.day24;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author ZYF
+ * @date: 2024/6/11 15:18
+ * @description:
+ */
+public class LE78 {
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 2};
+        List<List<Integer>> list = subsets(ints);
+        for (List<Integer> integers : list) {
+            System.out.println(Arrays.toString(integers.stream().mapToInt(i -> i).toArray()));
+        }
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> resultOne = new ArrayList<>();
+
+        // 排序
+        Arrays.sort(nums);
+
+        subsetsBFS(0, nums, resultOne, result);
+        return result;
+    }
+
+    /**
+     * 这个和之前的区别在于，如果转化成一颗树。之前只要求叶子节点。这个全部都要
+     * 而且还要不重复
+     * @param startIndex 起始index
+     */
+    private static void subsetsBFS(int startIndex, int[] nums, List<Integer> resultOne, List<List<Integer>> result) {
+        result.add(new ArrayList<>(resultOne));
+
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i - 1 >= startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            resultOne.add(nums[i]);
+            subsetsBFS(i + 1, nums, resultOne, result);
+            resultOne.remove(resultOne.size() - 1);
+        }
+    }
+}
---
+++ src/main/java/camp/day24/LE93.java
@@ -0,0 +1,100 @@
+package camp.day24;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author ZYF
+ * @date: 2024/6/11 14:08
+ * @description:
+ */
+public class LE93 {
+    public static void main(String[] args) {
+        List<String> list = restoreIpAddresses("25525511135");
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    public static List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+
+
+        if (s.length() < 4 || s.length() > 12){
+            return result;
+        }
+
+        restoreIpAddressesBFS(0, temp, s, result);
+        return result;
+    }
+
+    /**
+     *
+     * @param startIndex 开始Index
+     * @param s 输入的s
+     * @param result 切割的字符串集
+     */
+    private static void restoreIpAddressesBFS(int startIndex, List<String> temp, String s, List<String> result) {
+        if (startIndex >= s.length() && temp.size() == 4) {
+            String resultOne = String.join(".", temp);
+            result.add(resultOne);
+        }
+
+        for (int i = startIndex; i < s.length() && Integer.parseInt(s.substring(startIndex, i + 1)) < 256; i++) {
+            // 8 为 substring 的取值就在 1 - 3之间
+            String substring = s.substring(startIndex, i + 1);
+            if (substring.length() < 1 || substring.length() > 3) {
+                continue;
+            }
+            if (substring.length() > 1 && substring.startsWith("0")) {
+                continue;
+            }
+            temp.add(substring);
+            restoreIpAddressesBFS(i + 1, temp, s, result);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    class Solution {
+        List<String> result = new ArrayList<String>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        public List<String> restoreIpAddresses(String s) {
+            restoreIpAddressesHandler(s, 0, 0);
+            return result;
+        }
+
+        // number表示stringbuilder中ip段的数量
+        public void restoreIpAddressesHandler(String s, int start, int number) {
+            // 如果start等于s的长度并且ip段的数量是4，则加入结果集，并返回
+            if (start == s.length() && number == 4) {
+                result.add(stringBuilder.toString());
+                return;
+            }
+            // 如果start等于s的长度但是ip段的数量不为4，或者ip段的数量为4但是start小于s的长度，则直接返回
+            if (start == s.length() || number == 4) {
+                return;
+            }
+            // 剪枝：ip段的长度最大是3，并且ip段处于[0,255]
+            for (int i = start; i < s.length() && i - start < 3 && Integer.parseInt(s.substring(start, i + 1)) >= 0
+                    && Integer.parseInt(s.substring(start, i + 1)) <= 255; i++) {
+                // 如果ip段的长度大于1，并且第一位为0的话，continue
+                if (i + 1 - start > 1 && s.charAt(start) - '0' == 0) {
+                    continue;
+                }
+                stringBuilder.append(s.substring(start, i + 1));
+                // 当stringBuilder里的网段数量小于3时，才会加点；如果等于3，说明已经有3段了，最后一段不需要再加点
+                if (number < 3) {
+                    stringBuilder.append(".");
+                }
+                number++;
+                restoreIpAddressesHandler(s, i + 1, number);
+                number--;
+                // 删除当前stringBuilder最后一个网段，注意考虑点的数量的问题
+                stringBuilder.delete(start + number, i + number + 2);
+            }
+        }
+    }
+}
